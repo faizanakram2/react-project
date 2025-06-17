a547ba157d6c3b16125c6bedea5002c538aed55a
faizanakram2 <faizanakram076@icloud.com>
2025-06-17 17:00:03 +0500

    platform added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import Herosection from './components/Herosection';
-
+import Platforms from './components/Platforms';
 function App() {
   return (
     <div>
       <Navbar />
       <Herosection />
+      <Platforms />
     </div>
   );
 }

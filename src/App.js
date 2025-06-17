5835607ed70a55516ead1eb4f2085ac3e710e847
faizanakram2 <faizanakram076@icloud.com>
2025-06-17 16:51:19 +0500

    herosection

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import Navbar from './components/Navbar';
+import HeroSection from './components/Herosection';
 
 function App() {
   return (
     <div>
-      
       <Navbar />
+      <HeroSection />
     </div>
   );
 }

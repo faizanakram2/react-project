425ef7dcc38a14dd3addeed957a63f312c63206e
faizanakram2 <faizanakram076@icloud.com>
2025-06-17 17:23:48 +0500

    two more sections added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import './App.css';
 import Navbar from './components/Navbar';
 import Herosection from './components/Herosection';
 import Platforms from './components/Platforms';
+import Features from './components/Features';
+import TokenSaleSection from './components/TokenSales';
 function App() {
   return (
     <div>
       <Navbar />
       <Herosection />
       <Platforms />
+      <Features />
+      <TokenSaleSection />
     </div>
   );
 }

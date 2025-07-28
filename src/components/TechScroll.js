603d1e8115705427bd23f3a2ba4ec4f6ae4ca3be
faizanakram2 <faizanakram076@icloud.com>
2025-07-28 12:23:20 +0500

    new theme with more feature and removed techscroll add

--- src/components/TechScroll.js
+++ src/components/TechScroll.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const items = ['Outsourcing', 'Technology', 'Consulting'];
+const items = ['Strategy', 'Execution', 'Acceleration'];
 
 const TechScroll = () => {
   return (
